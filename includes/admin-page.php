24f9415b049db12acca1d7d9b2cd03892468c1ba
Robin Cornett <hello@robincornett.com>
2021-04-06 09:29:02 -0400

    Convert exporter to use batch export
    #4

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -38,14 +38,32 @@ function edd_dcg_page() {
 }
 
 function edd_dcg_admin_messages() {
-	global $edd_options;
+	$number = ! empty( $_GET['edd-number'] ) ? (int) $_GET['edd-number'] : false;
 
-	if ( isset( $_GET['edd-message'] ) && 'discounts_added' == $_GET['edd-message'] && current_user_can( 'manage_shop_discounts' ) ) {
-		 $url = admin_url( 'edit.php?post_type=download&edd-action=discount_codes_recent_export' );
-		 $message = $_GET['edd-number'] .' '. __( 'codes generated', 'edd_dcg' ) .'. <a href="'. $url .'">'. __( 'Export to CSV', 'edd_dcg' ) .'</a>';
-		 add_settings_error( 'edd-dcg-notices', 'edd-discounts-added', $message, 'updated' );
-		 settings_errors( 'edd-dcg-notices' );
+	if ( ! $number || ! current_user_can( 'manage_shop_discounts' ) ) {
+		return;
 	}
 
+	if ( empty( $_GET['edd-message'] ) || 'discounts_added' !== $_GET['edd-message'] ) {
+		return;
+	}
+	ob_start();
+	printf(
+		/* translators: the number of discount codes generated. */
+		esc_html__( '%s codes generated.', 'edd_dcg' ),
+		(int) $number
+	);
+	?>
+	<form id="edd-dcg-export" method="post" class="edd-export-form edd-import-export-form">
+		<?php wp_nonce_field( 'edd_ajax_export', 'edd_ajax_export' ); ?>
+		<input type="hidden" name="edd-dcg-recent" value="<?php echo ( (int) $number ); ?>"/>
+		<input type="hidden" name="edd-export-class" value="EDD_Discount_Codes_Export"/>
+		<input type="submit" value="<?php esc_html_e( 'Generate CSV', 'edd_dcg' ); ?>" class="button-secondary"/>
+		<span class="spinner"></span>
+	</form>
+	<?php
+	$message = ob_get_clean();
+	add_settings_error( 'edd-dcg-notices', 'edd-discounts-added', $message, 'updated' );
+	settings_errors( 'edd-dcg-notices' );
 }
 add_action( 'admin_notices', 'edd_dcg_admin_messages', 10 );
--- includes/class-export-discount-codes.php
+++ includes/class-export-discount-codes.php
@@ -7,103 +7,151 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
+class EDD_Discount_Codes_Export extends EDD_Batch_Export {
 
-class EDD_Discount_Codes_Export extends EDD_Export {
+	/**
+	 * Whether the export should include just the most recently generated discount codes.
+	 *
+	 * @var boolean
+	 */
+	private $recent = false;
 
-	private $recent;
+	/**
+	 * The export type.
+	 *
+	 * @var string
+	 */
 	public $export_type = 'discount_codes';
 
-	function __construct($recent = false) {
-		$this->recent = $recent;
-	}
-
+	/**
+	 * The columns for the CSV.
+	 *
+	 * @return array
+	 */
 	public function csv_cols() {
-		$cols = array(
-			'name'  	=> __( 'Name', 'edd_dcg' ),
-			'code'  	=> __( 'Code', 'edd_dcg' ),
-			'amount'  	=> __( 'Amount', 'edd_dcg' ),
-			'uses'  	=> __( 'Uses', 'edd_dcg' ),
-			'max_uses' 	=> __( 'Max Uses', 'edd_dcg' ),
-			'start_date'=> __( 'Start Date', 'edd_dcg' ),
-			'expiration'=> __( 'Expiration', 'edd_dcg' ),
-			'status'  	=> __( 'Status', 'edd_dcg' ),
+		return array(
+			'name'       => __( 'Name', 'edd_dcg' ),
+			'code'       => __( 'Code', 'edd_dcg' ),
+			'amount'     => __( 'Amount', 'edd_dcg' ),
+			'uses'       => __( 'Uses', 'edd_dcg' ),
+			'max_uses'   => __( 'Max Uses', 'edd_dcg' ),
+			'start_date' => __( 'Start Date', 'edd_dcg' ),
+			'expiration' => __( 'Expiration', 'edd_dcg' ),
+			'status'     => __( 'Status', 'edd_dcg' ),
 		);
-
-		return $cols;
 	}
 
+	/**
+	 * The data for the CSV.
+	 *
+	 * @return array
+	 */
 	public function get_data() {
 
-		$discount_codes_data = array();
-		$number = -1;
-		$args = array( 	'orderby'          => 'ID',
-						'order'            => 'DESC',
-						'posts_per_page' => $number
-					);
+		$data      = array();
+		$args      = array(
+			'orderby'        => 'ID',
+			'order'          => 'DESC',
+			'paged'          => $this->step,
+			'posts_per_page' => 100,
+		);
 		$discounts = edd_get_discounts( $args );
-		if ($this->recent) {
-			$number = 1;
-			$last = edd_get_discounts( $args );
-			$code_name = $last[0]->post_name;
-			$code_name = substr($code_name, 0, strpos($code_name, '-') + 1);
-			$last_discounts = array();
+		if ( $this->recent ) {
+			$args['posts_per_page'] = $this->recent;
+			$last                   = edd_get_discounts( $args );
+			$code_name              = $last[0]->post_name;
+			$code_name              = substr( $code_name, 0, strpos( $code_name, '-' ) + 1 );
+			$last_discounts         = array();
 			foreach ( $discounts as $discount ) {
-				if ( strpos($discount->post_name, $code_name) !== false ) {
+				if ( strpos( $discount->post_name, $code_name ) !== false ) {
 					$last_discounts[] = $discount;
 				}
 			}
 			$discounts = $last_discounts;
 		}
 
-		if ( $discounts ) {
-			foreach ( $discounts as $discount ) {
-				if ( edd_get_discount_max_uses( $discount->ID ) ) {
-					$uses =  edd_get_discount_uses( $discount->ID ) . '/' . edd_get_discount_max_uses( $discount->ID );
-				} else {
-					$uses = edd_get_discount_uses( $discount->ID );
-				}
-
-				if ( edd_get_discount_max_uses( $discount->ID ) ) {
-					$max_uses = edd_get_discount_max_uses( $discount->ID ) ? edd_get_discount_max_uses( $discount->ID ) : __( 'unlimited', 'edd_dcg' );
-				} else {
-					$max_uses = __( 'Unlimited', 'edd_dcg' );
-				}
-
-				$start_date = edd_get_discount_start_date( $discount->ID );
-
-				if ( ! empty( $start_date ) ) {
-					$discount_start_date =  date_i18n( get_option( 'date_format' ), strtotime( $start_date ) );
-				} else {
-					$discount_start_date = __( 'No start date', 'edd_dcg' );
-				}
-
-				if ( edd_get_discount_expiration( $discount->ID ) ) {
-					$expiration = edd_is_discount_expired( $discount->ID ) ? __( 'Expired', 'edd_dcg' ) : date_i18n( get_option( 'date_format' ), strtotime( edd_get_discount_expiration( $discount->ID ) ) );
-				} else {
-					$expiration = __( 'No expiration', 'edd_dcg' );
-				}
-
-				$discount_codes_data[] = array(
-					'ID' 			=> $discount->ID,
-					'name' 			=> get_the_title( $discount->ID ),
-					'code' 			=> edd_get_discount_code( $discount->ID ),
-					'amount' 		=> edd_format_discount_rate( edd_get_discount_type( $discount->ID ), edd_get_discount_amount( $discount->ID ) ),
-					'uses' 			=> $uses,
-					'max_uses' 		=> $max_uses,
-					'start_date' 	=> $discount_start_date,
-					'expiration'	=> $expiration,
-					'status'		=> ucwords( $discount->post_status ),
-				);
+		if ( ! $discounts ) {
+			return false;
+		}
+		foreach ( $discounts as $discount ) {
+			if ( edd_get_discount_max_uses( $discount->ID ) ) {
+				$uses = edd_get_discount_uses( $discount->ID ) . '/' . edd_get_discount_max_uses( $discount->ID );
+			} else {
+				$uses = edd_get_discount_uses( $discount->ID );
 			}
+
+			$max_uses = __( 'Unlimited', 'edd_dcg' );
+			if ( edd_get_discount_max_uses( $discount->ID ) ) {
+				$max_uses = edd_get_discount_max_uses( $discount->ID ) ? edd_get_discount_max_uses( $discount->ID ) : __( 'unlimited', 'edd_dcg' );
+			}
+
+			$start_date          = edd_get_discount_start_date( $discount->ID );
+			$discount_start_date = __( 'No start date', 'edd_dcg' );
+			if ( ! empty( $start_date ) ) {
+				$discount_start_date = date_i18n( get_option( 'date_format' ), strtotime( $start_date ) );
+			}
+
+			$expiration = __( 'No expiration', 'edd_dcg' );
+			if ( edd_get_discount_expiration( $discount->ID ) ) {
+				$expiration = edd_is_discount_expired( $discount->ID ) ? __( 'Expired', 'edd_dcg' ) : date_i18n( get_option( 'date_format' ), strtotime( edd_get_discount_expiration( $discount->ID ) ) );
+			}
+
+			$data[] = array(
+				'ID'         => $discount->ID,
+				'name'       => get_the_title( $discount->ID ),
+				'code'       => edd_get_discount_code( $discount->ID ),
+				'amount'     => edd_format_discount_rate( edd_get_discount_type( $discount->ID ), edd_get_discount_amount( $discount->ID ) ),
+				'uses'       => $uses,
+				'max_uses'   => $max_uses,
+				'start_date' => $discount_start_date,
+				'expiration' => $expiration,
+				'status'     => ucwords( $discount->post_status ),
+			);
 		}
 
-		$data = apply_filters( 'edd_export_get_data', $discount_codes_data );
-		$data = apply_filters( 'edd_export_get_data_' . $this->export_type, $discount_codes_data );
+		$data = apply_filters( 'edd_export_get_data', $data );
+		$data = apply_filters( 'edd_export_get_data_' . $this->export_type, $data );
 
-
-		return $discount_codes_data;
+		return $data;
 	}
 
+	/**
+	 * Gets the percentage of the completed job.
+	 *
+	 * @return void
+	 */
+	public function get_percentage_complete() {
+
+		$percentage = 100;
+		if ( $this->recent ) {
+			return $percentage;
+		}
+		$discounts = edd_get_discounts(
+			array(
+				'posts_per_page' => 999999,
+				'fields'         => 'ids',
+			)
+		);
+		$total     = count( $discounts );
+
+		if ( $total > 0 ) {
+			$percentage = ( ( 100 * $this->step ) / $total ) * 100;
+		}
+
+		if ( $percentage > 100 ) {
+			$percentage = 100;
+		}
+
+		return $percentage;
+	}
+
+	public function set_properties( $request ) {
+		if ( ! empty( $request['edd-dcg-recent'] ) ) {
+			$this->recent = (int) $request['edd-dcg-recent'];
+		}
+	}
 }
