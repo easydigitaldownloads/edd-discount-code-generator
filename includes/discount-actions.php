4b64aeb447de90c7b512592951343010fd2b53c6
LisaCee <lisacanini@sandhillsdev.com>
2021-05-27 09:12:16 -0700

    #11 Include end time with format string

--- includes/discount-actions.php
+++ includes/discount-actions.php
@@ -95,7 +95,7 @@ function edd_dcg_create_discount_codes( $data ) {
 	if ( function_exists( 'edd_add_adjustment' ) ) {
 		$code['scope'] = ! empty( $data['not_global'] ) ? 'not_global' : 'global';
 		if ( isset( $code['expiration'] ) ) {
-			$code['expiration'] = date( 'Y-m-d', strtotime( $code['expiration'] ) . '23:59:59' );
+			$code['expiration'] = date( 'Y-m-d 23:59:59', strtotime( $code['expiration'] ) );
 		}
 		foreach ( $fields_to_convert as $edd2x => $edd30 ) {
 			$code[ $edd30 ] = ! empty( $code[ $edd2x ] ) ? $code[ $edd2x ] : '';
